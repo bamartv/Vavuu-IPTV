380a525cd13f36c53c036a4b92aeedc12eadbfaa
Kostya Lolinger <74829881+SuperNova-Repo@users.noreply.github.com>
2026-05-03 00:41:34 +0200

    Add files via upload

---
+++ GETtings/Serien.js
@@ -0,0 +1,202 @@
+{
+  "js": [
+    {
+      "id": "*",
+      "title": "All",
+      "alias": "*",
+      "censored": 0
+    },
+    {
+      "id": "3153",
+      "title": "┃MULTI / LINGO┃ NETFLIX",
+      "alias": "┃MULTI / LINGO┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "106",
+      "title": "┃DE┃ NETFLIX",
+      "alias": "┃DE┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "484",
+      "title": "┃DE┃ RTL+",
+      "alias": "┃DE┃ RTL+",
+      "censored": 0
+    },
+    {
+      "id": "776",
+      "title": "┃DE┃ ZDF MEDIATHEK",
+      "alias": "┃DE┃ ZDF MEDIATHEK",
+      "censored": 0
+    },
+    {
+      "id": "775",
+      "title": "┃DE┃ ARD MEDIATHEK",
+      "alias": "┃DE┃ ARD MEDIATHEK",
+      "censored": 0
+    },
+    {
+      "id": "107",
+      "title": "┃DE┃ MAGENTA",
+      "alias": "┃DE┃ MAGENTA",
+      "censored": 0
+    },
+    {
+      "id": "778",
+      "title": "┃DE┃ TVNOW",
+      "alias": "┃DE┃ TVNOW",
+      "censored": 0
+    },
+    {
+      "id": "295",
+      "title": "┃DE┃ VIAPLAY",
+      "alias": "┃DE┃ VIAPLAY",
+      "censored": 0
+    },
+    {
+      "id": "108",
+      "title": "┃DE┃ AMAZONPRIME VIDEO",
+      "alias": "┃DE┃ AMAZONPRIME VIDEO",
+      "censored": 0
+    },
+    {
+      "id": "112",
+      "title": "┃DE┃ DISNEY+ | NATGEO",
+      "alias": "┃DE┃ DISNEY+ | NATGEO",
+      "censored": 0
+    },
+    {
+      "id": "779",
+      "title": "┃DE┃ FX",
+      "alias": "┃DE┃ FX",
+      "censored": 0
+    },
+    {
+      "id": "110",
+      "title": "┃DE┃ HBOMAX",
+      "alias": "┃DE┃ HBOMAX",
+      "censored": 0
+    },
+    {
+      "id": "111",
+      "title": "┃DE┃ APPLE TV+",
+      "alias": "┃DE┃ APPLE TV+",
+      "censored": 0
+    },
+    {
+      "id": "614",
+      "title": "┃DE┃ MGM+",
+      "alias": "┃DE┃ MGM+",
+      "censored": 0
+    },
+    {
+      "id": "487",
+      "title": "┃DE┃ DISCOVERY+",
+      "alias": "┃DE┃ DISCOVERY+",
+      "censored": 0
+    },
+    {
+      "id": "109",
+      "title": "┃DE┃ STARZPLAY",
+      "alias": "┃DE┃ STARZPLAY",
+      "censored": 0
+    },
+    {
+      "id": "299",
+      "title": "┃DE┃ HULU",
+      "alias": "┃DE┃ HULU",
+      "censored": 0
+    },
+    {
+      "id": "766",
+      "title": "┃DE┃ STARZ",
+      "alias": "┃DE┃ STARZ",
+      "censored": 0
+    },
+    {
+      "id": "780",
+      "title": "┃DE┃ JOYN",
+      "alias": "┃DE┃ JOYN",
+      "censored": 0
+    },
+    {
+      "id": "312",
+      "title": "┃DE┃ PEACOCK",
+      "alias": "┃DE┃ PEACOCK",
+      "censored": 0
+    },
+    {
+      "id": "300",
+      "title": "┃DE┃ SKY ORIGINAL",
+      "alias": "┃DE┃ SKY ORIGINAL",
+      "censored": 0
+    },
+    {
+      "id": "305",
+      "title": "┃DE┃ SHOWTIME",
+      "alias": "┃DE┃ SHOWTIME",
+      "censored": 0
+    },
+    {
+      "id": "777",
+      "title": "┃DE┃ WOWTV",
+      "alias": "┃DE┃ WOWTV",
+      "censored": 0
+    },
+    {
+      "id": "304",
+      "title": "┃DE┃ PARAMOUNT+",
+      "alias": "┃DE┃ PARAMOUNT+",
+      "censored": 0
+    },
+    {
+      "id": "139",
+      "title": "┃DE┃ BBC | USA SERIES",
+      "alias": "┃DE┃ BBC | USA SERIES",
+      "censored": 0
+    },
+    {
+      "id": "311",
+      "title": "┃DE┃ KINDER",
+      "alias": "┃DE┃ KINDER",
+      "censored": 0
+    },
+    {
+      "id": "306",
+      "title": "┃DE┃ BET+",
+      "alias": "┃DE┃ BET+",
+      "censored": 0
+    },
+    {
+      "id": "375",
+      "title": "┃DE┃ VIAPLAY",
+      "alias": "┃DE┃ VIAPLAY",
+      "censored": 0
+    },
+    {
+      "id": "428",
+      "title": "┃DE┃ KINDERSERIEN",
+      "alias": "┃DE┃ KINDERSERIEN",
+      "censored": 0
+    },
+    {
+      "id": "479",
+      "title": "┃DE┃ DOKU",
+      "alias": "┃DE┃ DOKU",
+      "censored": 0
+    },
+    {
+      "id": "781",
+      "title": "┃DE┃ ANIME",
+      "alias": "┃DE┃ ANIME",
+      "censored": 0
+    },
+    {
+      "id": "332",
+      "title": "┃GR┃ MGM+ | THE CW",
+      "alias": "┃GR┃ MGM+ | THE CW",
+      "censored": 0
+    }
+  ]
+}
---
+++ GETtings/vod.js
@@ -0,0 +1,454 @@
+{
+  "js": [
+    {
+      "id": "*",
+      "title": "All",
+      "alias": "*",
+      "censored": 0
+    },
+    {
+      "id": "294",
+      "title": "┃MULTI / LINGO┃ 4K HDR | SDR",
+      "alias": "┃MULTI / LINGO┃ 4K HDR | SDR",
+      "censored": 0
+    },
+    {
+      "id": "3709",
+      "title": "┃MULTI / LINGO┃ NEW RELEASES",
+      "alias": "┃MULTI / LINGO┃ NEW RELEASES",
+      "censored": 0
+    },
+    {
+      "id": "2741",
+      "title": "┃MULTI / LINGO┃ PARAMOUNT+",
+      "alias": "┃MULTI / LINGO┃ PARAMOUNT+",
+      "censored": 0
+    },
+    {
+      "id": "3701",
+      "title": "┃MULTI / LINGO┃ NETFLIX",
+      "alias": "┃MULTI / LINGO┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "3495",
+      "title": "┃MULTI / LINGO┃ JAMES BOND",
+      "alias": "┃MULTI / LINGO┃ JAMES BOND",
+      "censored": 0
+    },
+    {
+      "id": "3702",
+      "title": "┃MULTI / LINGO┃ AMAZON PRIME",
+      "alias": "┃MULTI / LINGO┃ AMAZON PRIME",
+      "censored": 0
+    },
+    {
+      "id": "3703",
+      "title": "┃MULTI / LINGO┃ HBOMAX",
+      "alias": "┃MULTI / LINGO┃ HBOMAX",
+      "censored": 0
+    },
+    {
+      "id": "3704",
+      "title": "┃MULTI / LINGO┃ DISNEY+",
+      "alias": "┃MULTI / LINGO┃ DISNEY+",
+      "censored": 0
+    },
+    {
+      "id": "3705",
+      "title": "┃MULTI / LINGO┃ SKYSHOWTIME",
+      "alias": "┃MULTI / LINGO┃ SKYSHOWTIME",
+      "censored": 0
+    },
+    {
+      "id": "3706",
+      "title": "┃MULTI / LINGO┃ APPLE+",
+      "alias": "┃MULTI / LINGO┃ APPLE+",
+      "censored": 0
+    },
+    {
+      "id": "3540",
+      "title": "┃MULTI / LINGO┃ KIDS",
+      "alias": "┃MULTI / LINGO┃ KIDS",
+      "censored": 0
+    },
+    {
+      "id": "270",
+      "title": "┃NL┃ ANIME",
+      "alias": "┃NL┃ ANIME",
+      "censored": 0
+    },
+    {
+      "id": "3637",
+      "title": "┃DE┃ TOP 100",
+      "alias": "┃DE┃ TOP 100",
+      "censored": 0
+    },
+    {
+      "id": "118",
+      "title": "┃DE┃ KINO",
+      "alias": "┃DE┃ KINO",
+      "censored": 0
+    },
+    {
+      "id": "119",
+      "title": "┃DE┃ KINO KINDER",
+      "alias": "┃DE┃ KINO KINDER",
+      "censored": 0
+    },
+    {
+      "id": "113",
+      "title": "┃DE┃ NETFLIX",
+      "alias": "┃DE┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "485",
+      "title": "┃DE┃ RTL+ ┃ NOWTV",
+      "alias": "┃DE┃ RTL+ ┃ NOWTV",
+      "censored": 0
+    },
+    {
+      "id": "114",
+      "title": "┃DE┃ MAGENTA",
+      "alias": "┃DE┃ MAGENTA",
+      "censored": 0
+    },
+    {
+      "id": "115",
+      "title": "┃DE┃ AMAZON PRIME",
+      "alias": "┃DE┃ AMAZON PRIME",
+      "censored": 0
+    },
+    {
+      "id": "117",
+      "title": "┃DE┃ DISNEY+ ┃ DREAMWORKS ┃ NATGEO",
+      "alias": "┃DE┃ DISNEY+ ┃ DREAMWORKS ┃ NATGEO",
+      "censored": 0
+    },
+    {
+      "id": "604",
+      "title": "┃DE┃ JOYN",
+      "alias": "┃DE┃ JOYN",
+      "censored": 0
+    },
+    {
+      "id": "116",
+      "title": "┃DE┃ HBOMAX",
+      "alias": "┃DE┃ HBOMAX",
+      "censored": 0
+    },
+    {
+      "id": "140",
+      "title": "┃DE┃ APPLE TV+",
+      "alias": "┃DE┃ APPLE TV+",
+      "censored": 0
+    },
+    {
+      "id": "819",
+      "title": "┃DE┃ BOLLYWOOD",
+      "alias": "┃DE┃ BOLLYWOOD",
+      "censored": 0
+    },
+    {
+      "id": "899",
+      "title": "┃DE┃ FILM COLLECTION",
+      "alias": "┃DE┃ FILM COLLECTION",
+      "censored": 0
+    },
+    {
+      "id": "426",
+      "title": "┃DE┃ FERNSEHEN MEDIATHEK",
+      "alias": "┃DE┃ FERNSEHEN MEDIATHEK",
+      "censored": 0
+    },
+    {
+      "id": "123",
+      "title": "┃DE┃ FAMILIE",
+      "alias": "┃DE┃ FAMILIE",
+      "censored": 0
+    },
+    {
+      "id": "122",
+      "title": "┃DE┃ KOMÖDIE",
+      "alias": "┃DE┃ KOMÖDIE",
+      "censored": 0
+    },
+    {
+      "id": "128",
+      "title": "┃DE┃ DRAMA ┃ ROMANTIK",
+      "alias": "┃DE┃ DRAMA ┃ ROMANTIK",
+      "censored": 0
+    },
+    {
+      "id": "120",
+      "title": "┃DE┃ AKTION",
+      "alias": "┃DE┃ AKTION",
+      "censored": 0
+    },
+    {
+      "id": "121",
+      "title": "┃DE┃ ABENTEUER",
+      "alias": "┃DE┃ ABENTEUER",
+      "censored": 0
+    },
+    {
+      "id": "134",
+      "title": "┃DE┃ KRIMI",
+      "alias": "┃DE┃ KRIMI",
+      "censored": 0
+    },
+    {
+      "id": "124",
+      "title": "┃DE┃ THRILLER ┃ GEHEIMNIS",
+      "alias": "┃DE┃ THRILLER ┃ GEHEIMNIS",
+      "censored": 0
+    },
+    {
+      "id": "129",
+      "title": "┃DE┃ HORROR",
+      "alias": "┃DE┃ HORROR",
+      "censored": 0
+    },
+    {
+      "id": "130",
+      "title": "┃DE┃ HISTORY ┃ KRIEG",
+      "alias": "┃DE┃ HISTORY ┃ KRIEG",
+      "censored": 0
+    },
+    {
+      "id": "125",
+      "title": "┃DE┃ SCIENCE FICTION",
+      "alias": "┃DE┃ SCIENCE FICTION",
+      "censored": 0
+    },
+    {
+      "id": "126",
+      "title": "┃DE┃ FANTASIE",
+      "alias": "┃DE┃ FANTASIE",
+      "censored": 0
+    },
+    {
+      "id": "132",
+      "title": "┃DE┃ KINDER",
+      "alias": "┃DE┃ KINDER",
+      "censored": 0
+    },
+    {
+      "id": "137",
+      "title": "┃DE┃ KABARETT ┃ KONZERT ┃ MUSIK",
+      "alias": "┃DE┃ KABARETT ┃ KONZERT ┃ MUSIK",
+      "censored": 0
+    },
+    {
+      "id": "136",
+      "title": "┃DE┃ DOKUMENTAR FILME",
+      "alias": "┃DE┃ DOKUMENTAR FILME",
+      "censored": 0
+    },
+    {
+      "id": "142",
+      "title": "┃DE┃ KOLLEKTIONEN",
+      "alias": "┃DE┃ KOLLEKTIONEN",
+      "censored": 0
+    },
+    {
+      "id": "131",
+      "title": "┃DE┃ WESTERN",
+      "alias": "┃DE┃ WESTERN",
+      "censored": 0
+    },
+    {
+      "id": "135",
+      "title": "┃DE┃ WEIHNACHTS FILME",
+      "alias": "┃DE┃ WEIHNACHTS FILME",
+      "censored": 0
+    },
+    {
+      "id": "621",
+      "title": "┃DE┃ ANIME",
+      "alias": "┃DE┃ ANIME",
+      "censored": 0
+    },
+    {
+      "id": "3632",
+      "title": "┃DE┃ SYLVESTER STALLONE",
+      "alias": "┃DE┃ SYLVESTER STALLONE",
+      "censored": 0
+    },
+    {
+      "id": "3603",
+      "title": "┃DE┃ NICOLAS CAGE",
+      "alias": "┃DE┃ NICOLAS CAGE",
+      "censored": 0
+    },
+    {
+      "id": "3630",
+      "title": "┃DE┃ RYAN REYNOLDS",
+      "alias": "┃DE┃ RYAN REYNOLDS",
+      "censored": 0
+    },
+    {
+      "id": "394",
+      "title": "┃FR┃ AMAZON PRIME",
+      "alias": "┃FR┃ AMAZON PRIME",
+      "censored": 0
+    },
+    {
+      "id": "409",
+      "title": "┃FR┃ COMEDIE",
+      "alias": "┃FR┃ COMEDIE",
+      "censored": 0
+    },
+    {
+      "id": "532",
+      "title": "┃EN┃ NETFLIX",
+      "alias": "┃EN┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "531",
+      "title": "┃EN┃ AMAZON PRIME",
+      "alias": "┃EN┃ AMAZON PRIME",
+      "censored": 0
+    },
+    {
+      "id": "805",
+      "title": "┃EN┃ HULU",
+      "alias": "┃EN┃ HULU",
+      "censored": 0
+    },
+    {
+      "id": "544",
+      "title": "┃EN┃ COMEDY",
+      "alias": "┃EN┃ COMEDY",
+      "censored": 0
+    },
+    {
+      "id": "545",
+      "title": "┃EN┃ DRAMA ┃ ROMANCE",
+      "alias": "┃EN┃ DRAMA ┃ ROMANCE",
+      "censored": 0
+    },
+    {
+      "id": "548",
+      "title": "┃EN┃ CRIME",
+      "alias": "┃EN┃ CRIME",
+      "censored": 0
+    },
+    {
+      "id": "540",
+      "title": "┃EN┃ ACTION",
+      "alias": "┃EN┃ ACTION",
+      "censored": 0
+    },
+    {
+      "id": "546",
+      "title": "┃EN┃ ADVENTURE",
+      "alias": "┃EN┃ ADVENTURE",
+      "censored": 0
+    },
+    {
+      "id": "549",
+      "title": "┃EN┃ FANTASY",
+      "alias": "┃EN┃ FANTASY",
+      "censored": 0
+    },
+    {
+      "id": "543",
+      "title": "┃EN┃ THRILLER┃MYSTERY",
+      "alias": "┃EN┃ THRILLER┃MYSTERY",
+      "censored": 0
+    },
+    {
+      "id": "542",
+      "title": "┃EN┃ HORROR",
+      "alias": "┃EN┃ HORROR",
+      "censored": 0
+    },
+    {
+      "id": "791",
+      "title": "┃EN┃ HISTORY┃WAR",
+      "alias": "┃EN┃ HISTORY┃WAR",
+      "censored": 0
+    },
+    {
+      "id": "811",
+      "title": "┃EN┃ MUSIC",
+      "alias": "┃EN┃ MUSIC",
+      "censored": 0
+    },
+    {
+      "id": "2838",
+      "title": "┃PL┃ POLSKIE FILM",
+      "alias": "┃PL┃ POLSKIE FILM",
+      "censored": 0
+    },
+    {
+      "id": "2837",
+      "title": "┃PL┃ FILMY 4K",
+      "alias": "┃PL┃ FILMY 4K",
+      "censored": 0
+    },
+    {
+      "id": "2836",
+      "title": "┃PL┃ OSTATNIO DODANE",
+      "alias": "┃PL┃ OSTATNIO DODANE",
+      "censored": 0
+    },
+    {
+      "id": "2841",
+      "title": "┃PL┃ NETFLIX",
+      "alias": "┃PL┃ NETFLIX",
+      "censored": 0
+    },
+    {
+      "id": "2845",
+      "title": "┃PL┃ DISNEY+",
+      "alias": "┃PL┃ DISNEY+",
+      "censored": 0
+    },
+    {
+      "id": "2647",
+      "title": "┃PL┃ AKCJA ┃SENSACJA ┃KRYMINAL",
+      "alias": "┃PL┃ AKCJA ┃SENSACJA ┃KRYMINAL",
+      "censored": 0
+    },
+    {
+      "id": "2842",
+      "title": "┃PL┃ DRAMA ┃ROMANSOWE",
+      "alias": "┃PL┃ DRAMA ┃ROMANSOWE",
+      "censored": 0
+    },
+    {
+      "id": "2840",
+      "title": "┃PL┃ KOMEDIA",
+      "alias": "┃PL┃ KOMEDIA",
+      "censored": 0
+    },
+    {
+      "id": "2891",
+      "title": "┃PL┃ FAMILIJNE",
+      "alias": "┃PL┃ FAMILIJNE",
+      "censored": 0
+    },
+    {
+      "id": "2839",
+      "title": "┃PL┃ FILMY DLA DZIECI",
+      "alias": "┃PL┃ FILMY DLA DZIECI",
+      "censored": 0
+    },
+    {
+      "id": "2847",
+      "title": "┃PL┃ HORROR",
+      "alias": "┃PL┃ HORROR",
+      "censored": 0
+    },
+    {
+      "id": "2846",
+      "title": "┃PL┃ DOKUMENTALNE",
+      "alias": "┃PL┃ DOKUMENTALNE",
+      "censored": 0
+    }
+  ]
+}
